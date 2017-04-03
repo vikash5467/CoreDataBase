c0f0b3e507a773d5fd21908ffe53741e647df86a
vikash kumar <vikash@nineleaps.com>
2017-04-03 15:42:52 +0530

    Initial Commit

---
+++ CoreDataBaseTests/CoreDataBaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataBaseTests.swift
+//  CoreDataBaseTests
+//
+//  Created by vikash kumar on 4/3/17.
+//  Copyright © 2017 vikash kumar. All rights reserved.
+//
+
+import XCTest
+@testable import CoreDataBase
+
+class CoreDataBaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
